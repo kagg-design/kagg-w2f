b83fdc29bcb355ed182348d5aadbe4c34e105678
KAGG Design <info@kagg.eu>
2021-07-24 16:48:57 +0300

    Add ban via nginx.

--- src/php/Main.php
+++ src/php/Main.php
@@ -14,6 +14,7 @@ use wfUtils;
  */
 class Main {
 	private const UPDATE_BAN_HOOK = 'kagg_w2f_update_ban';
+	private const NGINX_CONF      = '/etc/nginx/conf.d/block-ip-kagg-w2f.conf';
 
 	/**
 	 * Interval days.
@@ -62,38 +63,31 @@ class Main {
 	}
 
 	/**
-	 * Ban IPs collected by Wordfence and transfer them to Fail2ban.
+	 * Ban IPs collected by Wordfence and send them to Fail2ban and Nginx.
 	 */
 	public function update_ban_action(): void {
-		global $wpdb;
+		$blocked_ips = $this->get_blocked_ips();
 
-		if ( ! is_plugin_active( 'wordfence/wordfence.php' ) ) {
+		if ( ! $blocked_ips ) {
 			return;
 		}
 
-		// Get info on last interval_days.
-		$unix_day = (int) ( gmdate( 'U' ) / DAY_IN_SECONDS ) - $this->interval_days;
+		$this->update_fail2ban( $blocked_ips );
+		$this->update_nginx( $blocked_ips );
+	}
 
-		// phpcs:disable WordPress.DB.DirectDatabaseQuery.DirectQuery
-		// phpcs:disable WordPress.DB.DirectDatabaseQuery.NoCaching
-		$rows = $wpdb->get_results(
-			$wpdb->prepare( "SELECT * FROM {$wpdb->base_prefix}wfBlockedIPLog WHERE unixday > %s", $unix_day )
+	/**
+	 * Ban IPs collected by Wordfence and send them to Fail2ban.
+	 *
+	 * @param array $blocked_ips Blocked IPs.
+	 */
+	public function update_fail2ban( array $blocked_ips ): void {
+		$ban_commands = array_map(
+			static function ( $blocked_ip ) {
+				return 'sudo fail2ban-client set ssh-iptables banip ' . $blocked_ip;
+			},
+			$blocked_ips
 		);
-		// phpcs:enable WordPress.DB.DirectDatabaseQuery.NoCaching
-		// phpcs:enable WordPress.DB.DirectDatabaseQuery.DirectQuery
-
-		if ( ! $rows ) {
-			return;
-		}
-
-		$ban_commands = [];
-
-		foreach ( $rows as $row ) {
-			// phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
-			$ip = wfUtils::inet_ntop( $row->IP );
-
-			$ban_commands[] = 'fail2ban-client set ssh-iptables banip ' . $ip;
-		}
 
 		$ban_commands   = array_unique( $ban_commands );
 		$ban_commands[] = 'iptables-save | uniq | iptables-restore';
@@ -110,5 +104,103 @@ class Main {
 		exec( $cmd_filename );
 
 		unlink( $cmd_filename );
+	}
+
+	/**
+	 * Ban IPs collected by Wordfence and send them to Nginx.
+	 *
+	 * @param array $blocked_ips Blocked IPs.
+	 */
+	public function update_nginx( array $blocked_ips ): void {
+		$nginx_blocked_ips = [];
+
+		// phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
+		$config                  = (string) file_get_contents( self::NGINX_CONF );
+		$nginx_blocked_ips_found = preg_match(
+			'/(map \$real_ip \$allow {\n\tdefault true;\n)(.+?)}/s',
+			$config,
+			$matches
+		);
+
+		if ( $nginx_blocked_ips_found ) {
+			$nginx_blocked_ips = explode( "\n", $matches[2] );
+			$nginx_blocked_ips = array_map(
+				static function ( $nginx_blocked_ip ) {
+					return trim( str_replace( 'false', '', $nginx_blocked_ip ) );
+				},
+				$nginx_blocked_ips
+			);
+
+		}
+
+		$blocked_ips = array_unique( array_merge( $blocked_ips, $nginx_blocked_ips ) );
+
+		$ban_lines = implode(
+			"\n",
+			array_map(
+				static function ( $blocked_ip ) {
+					return "\t" . $blocked_ip . ' false;';
+				},
+				$blocked_ips
+			)
+		);
+
+		$config = 'map $http_x_forwarded_for $remote_ip {
+	default $http_x_forwarded_for;
+	"" $remote_addr;
+}
+
+map $remote_ip $real_ip {
+    default "";
+    "~^(?<first>.*),.*$"  $first;
+}
+
+map $real_ip $allow {
+    default true;
+' . $ban_lines . '
+}
+';
+
+		// phpcs:ignore WordPress.WP.AlternativeFunctions.file_system_read_file_put_contents
+		file_put_contents( self::NGINX_CONF, $config );
+
+		// phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.system_calls_exec
+		exec( 'sudo systemctl restart nginx' );
+	}
+
+	/**
+	 * Get blocked IPs from Wordfence.
+	 *
+	 * @return array
+	 */
+	private function get_blocked_ips(): array {
+		global $wpdb;
+
+		if ( ! is_plugin_active( 'wordfence/wordfence.php' ) ) {
+			return [];
+		}
+
+		// Get info on last interval_days.
+		$unix_day = (int) ( gmdate( 'U' ) / DAY_IN_SECONDS ) - $this->interval_days;
+
+		// phpcs:disable WordPress.DB.DirectDatabaseQuery.DirectQuery
+		// phpcs:disable WordPress.DB.DirectDatabaseQuery.NoCaching
+		$rows = $wpdb->get_results(
+			$wpdb->prepare( "SELECT * FROM {$wpdb->base_prefix}wfBlockedIPLog WHERE unixday > %s", $unix_day )
+		);
+		// phpcs:enable WordPress.DB.DirectDatabaseQuery.NoCaching
+		// phpcs:enable WordPress.DB.DirectDatabaseQuery.DirectQuery
+
+		if ( ! $rows ) {
+			return [];
+		}
+
+		return array_map(
+			static function ( $row ) {
+				// phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+				return wfUtils::inet_ntop( $row->IP );
+			},
+			$rows
+		);
 	}
 }
